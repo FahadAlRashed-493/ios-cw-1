57bbb9d05c1f9381d20eae052641532da18bb8f5
Faarashed <alrashed.fr@gmail.com>
2020-06-13 20:03:16 +0300

    done

---
+++ Cw1-fahad al rashed.playground/Contents.swift
@@ -0,0 +1,33 @@
+//classwork 1
+
+let name = "fahad"
+
+var hobby1 = "videogames"
+var hobby2 = "squash"
+var age = 16
+var  height = 1.7
+
+print ("hello my name is \(name), i like to play \(hobby1) and \(hobby2), my age is \(age) and my height is \(height)cm.")
+
+//my friend
+let friendname = "saleh"
+
+var friendhobby1 = "basketball"
+var friendhobby2 = "drwaing"
+var friendage = 15
+var friendheight = 1.8
+
+var age_diffrence = age - friendage
+
+// compare
+age > friendage
+age < friendage
+height <= friendheight
+friendheight >= height
+age == friendage
+friendheight == height
+friendage != age
+height != friendheight
+hobby2 == friendhobby2
+hobby1 == friendhobby1
+
